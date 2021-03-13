87c45ff6848a881a7c9ef794e7c7d0d3d3272b5d
StevenMartinez1 <steven.martinez0604@gmail.com>
2021-03-12 22:56:50 -0800

    Removed body-parser from npm packages

--- app.js
+++ app.js
@@ -10,16 +10,14 @@ const api_key = process.env.YELP_API_KEY;
 
 const client = yelp.client(api_key);
 
-
 function get_yelp_request(search_request){
-  client.search(search_request)
-  .then(function(response){
-      console.log(response.jsonBody["businesses"].length);
-      return response.jsonBody;
-    })
-    .catch((error) => {
-      console.log(error);
-  });
+  return client.search(search_request)
+   .then(function(response){
+       return response.jsonBody;
+     })
+     .catch((error) => {
+       console.log(error);
+   });
 }
 
 
@@ -35,17 +33,18 @@ app.post('/submit', function(req,res){
   const search_request = {
     term: req.body.food_category,
     location: req.body.location,
-    limit: 10
+    limit: 1
   };
 
-  //restaurants = get_yelp_request(search_request);\
-  restaurants = get_yelp_request(search_request);
+  restaurants = get_yelp_request(search_request).then(function(res){
+    //console.log(res.businesses);
+    return res.businesses;
+  });
 
-  //console.log(restaurants)
-  console.log("Yes/n/n/n/n");
+  console.log(restaurants);
 
-  //res.send(restaurants);
-  res.json(restaurants)
+
+  res.send(restaurants);
 });
 
 app.listen(3000);
